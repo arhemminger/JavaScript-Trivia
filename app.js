203479d51ee7e578c950bdfb1e393c569b527f41
Jordan Hennessy <jorrdygirl@me.com>
2019-03-31 21:11:03 -0500

    Styling done

--- app.js
+++ app.js
@@ -7,17 +7,14 @@
 ;===========================================
 */
 // Rank functions
-function openRank1() {
+function openRank() {
     document.getElementById('rank-container').style.display = "block";
     document.getElementById('home-container').style.display = "none";
     document.getElementById('quiz-container').style.display = "none";
     document.getElementById('bio-container').style.display = "none";
+    document.getElementById('title').style.display = "none";
 };
 
-function openRank2() {
-    document.getElementById('rank-container').style.display = "block";
-    document.getElementById('quiz-container').style.display = "none";
-};
 
 // Bio functions
 function openBio() {
@@ -25,6 +22,32 @@ function openBio() {
     document.getElementById('home-container').style.display = "none";
     document.getElementById('quiz-container').style.display = "none";
     document.getElementById('rank-container').style.display = "none";
+    document.getElementById('title').style.display = "none";
+    document.getElementById('jordanBio').style.display = "none";
+    document.getElementById('gabrielBio').style.display = "none";
+    document.getElementById('andrewBio').style.display = "none";
+    document.getElementById('group-bio').style.display = "block";
+};
+
+function andrewOpen() {
+    document.getElementById('group-bio').style.display = "none";
+    document.getElementById('andrewBio').style.display = "block";
+    document.getElementById('jordanBio').style.display = "none";
+    document.getElementById('gabrielBio').style.display = "none";
+};
+
+function jordanOpen() {
+    document.getElementById('group-bio').style.display = "none";
+    document.getElementById('jordanBio').style.display = "block";
+    document.getElementById('gabrielBio').style.display = "none";
+    document.getElementById('andrewBio').style.display = "none";
+};
+
+function gabrielOpen() {
+    document.getElementById('group-bio').style.display = "none";
+    document.getElementById('gabrielBio').style.display = "block";
+    document.getElementById('jordanBio').style.display = "none";
+    document.getElementById('andrewBio').style.display = "none";
 };
 
 // Home functions
@@ -33,6 +56,7 @@ function openHome() {
     document.getElementById('rank-container').style.display = "none";
     document.getElementById('quiz-container').style.display = "none";
     document.getElementById('bio-container').style.display = "none";
+    document.getElementById('title').style.display = "block";
 };
 // Quiz functions
 function openQuiz() {
@@ -40,6 +64,7 @@ function openQuiz() {
     document.getElementById('home-container').style.display = "none";
     document.getElementById('rank-container').style.display = "none";
     document.getElementById('bio-container').style.display = "none";
+    document.getElementById('title').style.display = "none";
 };
 
 function startQuiz() {
